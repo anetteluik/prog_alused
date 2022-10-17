c31a3aef2bee529bfbe243bb27db1881a3461b14
Anette Luik <anette.luik@voco.ee>
2022-10-17 22:02:47 +0300

    Presidendi ja astendamise ülesanne

---
+++ andmetyybid_muutujad/app.js
@@ -0,0 +1,17 @@
+// 1.1 Tervitus
+console.log("Tere, Maailm!")
+
+// 1.2 President
+let aasta = 2016;
+let president = "Kersti Kaljulaid";
+let lauseKeskosa = ". aastal valiti Eesti presidendiks ";
+let lause = aasta + lauseKeskosa + president;
+
+console.log(lause)
+
+// 1.3 Astendamine
+let x = prompt("Sisestage astme alus")
+let y = prompt("Sisestage astendaja")
+let xy = Math.pow(x,y)
+
+console.log(xy)
